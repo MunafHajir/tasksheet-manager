4eff917f7d4360a60d76b4bfd162e605b16c03c7
Pratik778899 <pratik78877@gmail.com>
2024-01-05 14:27:42 +0530

    Added Redux persist  and  jss

---
+++ tasksheet-manager/src/Routes/Routes.js
@@ -0,0 +1,15 @@
+import Layout from 'Layout'
+import React from 'react'
+import { Route , Routes } from 'react-router-dom'
+
+const Routes = () => {
+  return (
+   <Layout>
+    <Routes>
+        {/* <Route path='/Dashboard' element={}/> */}
+    </Routes>
+   </Layout>
+  )
+}
+
+export default Routes
---
+++ tasksheet-manager/src/Stores/Reducer/RootReducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import Reducer from "Stores/Reducer/Reducer"
+
+export const RootReducer = combineReducers({
+    Reducer
+})
