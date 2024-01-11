8d2a156f8db3af208d0537b7233e62e78b23125b
Vrutesh Mayekar <vruteshpm02@gmail.com>
2024-01-11 14:19:36 +0530

    fix: formated docs

--- tasksheet-manager/src/Pages/User_Pages/UserTable/CurrentUserTable.jsx
+++ tasksheet-manager/src/Pages/User_Pages/UserTable/CurrentUserTable.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { styled } from "@mui/material/styles";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -11,150 +11,142 @@ import { createUseStyles } from "react-jss";
 import Button from "../../../Components/Common/MuiButton";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
-
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-      color: theme.palette.common.black,
-    },
-    [`&.${tableCellClasses.body}`]: {
-      fontSize: 14,
-    },
-  }));
-  
-  const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    // hide last border
-    "&:last-child td, &:last-child th": {
-      border: 0,
-    },
-  }));
-  
-  function createData(modulename, task, eta, carbs, protein) {
-    return { modulename, task, eta, carbs, protein };
-  }
-  
-  const rows = [
-    createData(
-      "Developer Module",
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione eius maxime nulla explicabo ad, perspiciatis sapiente unde expedita quia vel atque quo aperiam delectus sunt et quis ducimus necessitatibus quas!",
-      "20h"
-    ),
-    createData(
-      "",
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione eius maxime nulla explicabo ad,!",
-      ""
-    ),
-    createData(
-      "Superadmin module",
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione eius",
-      "12h"
-    ),
-    createData(
-      "",
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, laborum?",
-      ""
-    ),
-    createData(
-      "Sales Manager module",
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione eius maxime nulla explicabo ad, perspiciatis sapiente unde expedita quia vel atque",
-      "15h"
-    ),
-    createData(
-      "",
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, laborum?",
-      ""
-    ),
-  ];
-  
-  const useStyles = createUseStyles({
-    headingcell: {
-      padding: "12px 24px",
-      backgroundColor: "#DCECFE",
-    },
-    modulecell: {
-      padding: "20px 24px",
-      width:"200px",
-    },
-    etacell: {
-      padding: "20px 24px",
-    },
-    tabcell: {
-      padding: "20px 24px ",
-    },
-    statuscell: {
-      padding: "20px 24px ",
-      width:"165px"
-    },
-    table:{
-        width:"87vw",
-    },
-    tablesection:{
-        boxShadow: "none", 
-        display:"flex", 
-        justifyContent:"center"
-    },
-  });
-  
-  export default function CustomizedTables() {
-    const classes = useStyles();
-    return (
-      <TableContainer component={Paper} className={classes.tablesection}>
-        <Table
-          className={classes.table}
-          aria-label="customized table"
-        >
-          <TableHead
-            className={classes.headingcell}
-            
-          >
-            <TableRow>
-              <StyledTableCell className={classes.headingcell}>
-                Module name
+  [`&.${tableCellClasses.head}`]: {
+    color: theme.palette.common.black,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
+function createData(modulename, task, eta, carbs, protein) {
+  return { modulename, task, eta, carbs, protein };
+}
+
+const rows = [
+  createData(
+    "Developer Module",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione eius maxime nulla explicabo ad, perspiciatis sapiente unde expedita quia vel atque quo aperiam delectus sunt et quis ducimus necessitatibus quas!",
+    "20h"
+  ),
+  createData(
+    "",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione eius maxime nulla explicabo ad,!",
+    ""
+  ),
+  createData(
+    "Superadmin module",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione eius",
+    "12h"
+  ),
+  createData(
+    "",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, laborum?",
+    ""
+  ),
+  createData(
+    "Sales Manager module",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione eius maxime nulla explicabo ad, perspiciatis sapiente unde expedita quia vel atque",
+    "15h"
+  ),
+  createData(
+    "",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, laborum?",
+    ""
+  ),
+];
+
+const useStyles = createUseStyles({
+  headingcell: {
+    padding: "12px 24px",
+    backgroundColor: "#DCECFE",
+  },
+  modulecell: {
+    padding: "20px 24px",
+    width: "200px",
+  },
+  etacell: {
+    padding: "20px 24px",
+  },
+  tabcell: {
+    padding: "20px 24px ",
+  },
+  statuscell: {
+    padding: "20px 24px ",
+    width: "165px",
+  },
+  table: {
+    width: "87vw",
+  },
+  tablesection: {
+    boxShadow: "none",
+    display: "flex",
+    justifyContent: "center",
+  },
+});
+
+export default function CustomizedTables() {
+  const classes = useStyles();
+  return (
+    <TableContainer component={Paper} className={classes.tablesection}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead></TableHead>
+        <TableCell></TableCell>
+        <TableHead className={classes.headingcell}>
+          <TableRow>
+            <StyledTableCell className={classes.headingcell}>
+              Module name
+            </StyledTableCell>
+            <StyledTableCell className={classes.headingcell} align="left">
+              Task
+            </StyledTableCell>
+            <StyledTableCell className={classes.headingcell} align="left">
+              ETA
+            </StyledTableCell>
+            <StyledTableCell className={classes.headingcell} align="left">
+              Status
+            </StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.modulename}>
+              <StyledTableCell
+                className={classes.modulecell}
+                component="th"
+                scope="row"
+              >
+                {row.modulename}
               </StyledTableCell>
-              <StyledTableCell className={classes.headingcell} align="left">
-                Task
+              <StyledTableCell className={classes.tabcell} align="left">
+                {row.task}
               </StyledTableCell>
-              <StyledTableCell className={classes.headingcell} align="left">
-                ETA
+              <StyledTableCell className={classes.etacell} align="left">
+                {row.eta}
               </StyledTableCell>
-              <StyledTableCell className={classes.headingcell} align="left">
-                Status
+              <StyledTableCell align="left" className={classes.statuscell}>
+                <Button
+                  btnsize="small"
+                  variant="outlined"
+                  btnheading="In progress"
+                  btnprops={{
+                    textTransform: "capitalize",
+                  }}
+                  btnicon={<KeyboardArrowDownIcon />}
+                />
               </StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <StyledTableRow key={row.modulename}>
-                <StyledTableCell
-                  className={classes.modulecell}
-                  component="th"
-                  scope="row"
-                >
-                  {row.modulename}
-                </StyledTableCell>
-                <StyledTableCell className={classes.tabcell} align="left">
-                  {row.task}
-                </StyledTableCell>
-                <StyledTableCell className={classes.etacell} align="left">
-                  {row.eta}
-                </StyledTableCell>
-                <StyledTableCell align="left" className={classes.statuscell}>
-                  <Button
-                    btnsize="small"
-                    variant="outlined"
-                    btnheading="In progress"
-                    btnprops={{
-                      textTransform: "capitalize",
-                     
-                     
-                    }}
-                    btnicon={<KeyboardArrowDownIcon />}
-                  />
-                </StyledTableCell>
-              </StyledTableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    );
-  }
-  
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
